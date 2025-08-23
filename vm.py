c43d24f7ef763581adab7640938b2250c59e1a8f
ConsciousCode <conscious.code@gmail.com>
2025-08-22 23:50:27 -0600

    Implemented canonical assembly with labels and ensured it worked

--- vm.py
+++ vm.py
@@ -1,6 +1,8 @@
 from itertools import batched
 import re
-from typing import Iterable
+from typing import DefaultDict, Iterable
+
+MAX_IMM = (1 << 25) - 1
 
 ASMRE = re.compile(r"""
     \s*(?:
@@ -8,12 +10,22 @@ ASMRE = re.compile(r"""
         (?P<op>\w+)\s*
         (?P<args>[^;]*)
     )?
-    (; (?P<comment>.*))?
+    (?:; (?P<comment>.*))?
 """, re.VERBOSE)
 
 CHARRE = re.compile(r"^'(?P<char>.)'$")
 
-OPCODES: dict[str, int] = { # name: registers
+CANONICAL = [
+    "cmov",
+    "aidx", "aupd",
+    "add", "mul", "div", "nand",
+    "halt",
+    "alloc", "dealloc",
+    "out", "in",
+    "loadprog", "loadimm"
+]
+
+ARGC: dict[str, int] = { # name: argc
     "mov": 3,
     "lda": 3, "sta": 3,
     "add": 3, "mul": 3, "div": 3, "nand": 3,
@@ -22,53 +34,105 @@ OPCODES: dict[str, int] = { # name: registers
     "out": 1, "inp": 1,
     "prg": 2, "ldi": 2
 }
-NAMES = list(OPCODES.keys())
-CODES = dict(enumerate(OPCODES.items()))
-
-def codeof(name: str):
-    try: return NAMES.index(name.lower())
-    except ValueError:
-        return "invalid"
+NAMES = list[str](ARGC.keys())
+CODES = dict[str, int]()
+for i, (canon, (name, argc)) in enumerate(zip(CANONICAL, list(ARGC.items()))):
+    CODES[canon] = i
+    CODES[name] = i
+    ARGC[canon] = argc
 
 def parse_value(value: str):
     if m := CHARRE.match(value):
         return ord(m['char'])
     return int(value, 0)
 
-def opasm(line: str):
-    if (m := ASMRE.match(line)) is None:
-        raise ValueError(f"Invalid assembly line: {line.strip()}")
-
-    if (op := m['op']) is None:
-        if inv := m['inv']:
-            return int(inv, 16)
-        return None  # Empty line or comment
-    
-    op = op.lower()
-    argc = OPCODES[op]
-    if m['args']:
-        args = [parse_value(arg) for arg in m['args'].split(',')]
-    else:
-        args = []
-    if len(args) != argc:
-        raise ValueError(f"Invalid number of arguments for {op}: {len(args)} ({argc} expected)")
-    
-    code = NAMES.index(op) << 28
-    if op == "ldi":
-        reg, imm = args
-        code |= (int(reg) & 0x07) << 24
-        code |= int(imm) & 0x01FF_FFFF
-    else:
-        for i, arg in enumerate(args):
-            code |= int(arg) << (3*i)
-    
-    return code
-
 def progasm(lines: Iterable[str]):
+    symtab = dict[str, int]() # label: address
+    patches = DefaultDict[str, set[int]](set) # label: addrs to patch
+    data = list[int]()
+
     for line in lines:
-        if line := line.strip():
-            if (op := opasm(line)) is not None:
-                yield op
+        if not (line := line.strip()):
+            continue
+        
+        if (m := ASMRE.match(line)) is None:
+            raise ValueError(f"Invalid assembly line: {line.strip()}")
+
+        if (op := m['op']) is None:
+            if inv := m['inv']:
+                code = int(inv, 16)
+            else:
+                continue
+        else:
+            op = op.lower()
+            if m['args']:
+                args = re.split(r"\s+", m['args'].strip())
+            else:
+                args = []
+            
+            if op in {"ldi", "loadimm"}:
+                reg, imm = args
+                if imm.startswith("@"):
+                    imm = imm[1:]
+                    if (addr := symtab.get(imm)) is not None:
+                        imm = addr
+                    else:
+                        patches[imm].add(len(data))
+                        imm = 0 # Placeholder
+                else:
+                    imm = parse_value(imm)
+                
+                code = 13 << 28 | (int(reg) & 7) << 25 | int(imm) & MAX_IMM
+            elif op == "label":
+                label, = args
+                if not label.startswith("@"):
+                    raise ValueError(f"Invalid label: {label}")
+                
+                label = label[1:]  # Remove '@' prefix
+                if label in symtab:
+                    raise ValueError(f"Duplicate label: {label}")
+                symtab[label] = len(data)
+                continue
+            else:
+                argc = ARGC[op]
+                if len(args) != argc:
+                    raise ValueError(
+                        "Invalid number of arguments for "
+                        f"{op}: {len(args)} ({argc} expected)"
+                    )
+                
+                try:
+                    code = CODES[op] << 28
+                except KeyError:
+                    raise ValueError(f"Unknown instruction: {op}") from None
+                
+                for i, arg in enumerate(reversed(args)):
+                    if arg.startswith("@"):
+                        label = arg[1:]
+                        if label in symtab:
+                            arg = symtab[label]
+                        else:
+                            patches[label].add(len(data))
+                            arg = 0
+                    else:
+                        arg = parse_value(arg)
+                    
+                    code |= (arg & 7) << (3*i)
+
+            data.append(code)
+        
+        if len(data) > MAX_IMM:
+            raise ValueError("Program size exceeds 25-bit limit")
+    
+    for label, addrs in patches.items():
+        if label not in symtab:
+            print(symtab)
+            raise ValueError(f"Undefined label: @{label}")
+        addr = symtab[label]
+        for a in addrs:
+            data[a] |= addr
+
+    return data
 
 def main_asm(inp, out):
     try:
@@ -83,14 +147,20 @@ def main_asm(inp, out):
     
     return 0
 
+def is_canonical(code: int):
+    # ldi is always canonical because it uses all bits
+    if code >> 28 == 13: return True
+    return not (code & (MAX_IMM ^ ((1 << 9) - 1)))
+
 def opdis(code: int):
     op = code >> 28
     if op == 13:
-        return ("ldi", (code >> 24) & 7, code & 0x01FF_FFFF)
-    
-    if op in CODES:
+        return ("ldi", (code >> 25) & 7, code & MAX_IMM)
+
+    if op < 13:
         abc = (code >> 6) & 7, (code >> 3) & 7, code & 7
-        name, count = CODES[op]
+        name = NAMES[op]
+        count = ARGC[name]
         return (name, abc[3 - count:])
     
     return code
@@ -98,22 +168,27 @@ def opdis(code: int):
 def main_dis(fname):
     try:
         with open(fname, 'rb') as file:
-            code = file.read()
+            prog = file.read()
     except FileNotFoundError:
         print(f"File {fname} not found.")
         return 1
     
-    for a, b, c, d in batched(code, 4):
-        match opdis(a<<24 | b<<16 | c<<8 | d):
+    for a, b, c, d in batched(prog, 4):
+        code = a<<24 | b<<16 | c<<8 | d
+        match opdis(code):
             case int(code):
-                print(f"0x{code:08x} ; invalid opcode")
+                print(f"0x{code:08x}")
             case ("ldi", reg, imm):
                 if 0x20 <= imm < 0x7f:
-                    print(f"ldi {reg}, 0x{imm:02x} ; '{chr(imm)}'")
+                    print(f"ldi {reg} 0x{imm:02x} ; '{chr(imm)}'")
                 else:
-                    print(f"ldi {reg}, 0x{imm:02x}")
+                    print(f"ldi {reg} 0x{imm:02x}")
             case (name, regs):
-                print(f"{name} {', '.join(map(str, regs))}")
+                line = f"{name} {' '.join(map(str, regs))}"
+                if is_canonical(code):
+                    print(line)
+                else:
+                    print(f"0x{code:08x} ; {line}")
 
     return 0
 
